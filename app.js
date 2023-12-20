7b523a5f4a8594f37ef576503184354c68d6cb5d
Hachounet <hachounet.github@gmx.fr>
2023-12-20 14:37:26 +0100

    V0.9
    Almost finished
    Need to work on ¹⁶ display

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let operator = "";
 let b = Infinity;
 let result = 0;
 const buttons = document.querySelectorAll('.buttons');
-let screenDisplay = document.getElementById('displayscreen')
+let screenDisplay = document.getElementById('displayscreen');
 
 function add(a, b) {
     let result = a + b;
@@ -18,11 +18,19 @@ function reduce(a, b) {
 function multiply(a, b) {
     let result = a * b;
     return result;
-
 }
 
 function divide(a, b) {
-    let result = a / b;
+    if (b === 0) {
+        return result = "Aborted. No div by 0";
+    } else {
+        let result = a / b;
+        return result;
+    }
+}
+
+function remainder(a, b) {
+    let result = (a % b);
     return result;
 }
 
@@ -33,65 +41,144 @@ function operate() {
 
     if (operatorInput === "+") {
         result = add(Number(aInput), Number(bInput));
-        a = result; // Réassigne le résultat actuel à la variable a
-        b = Infinity;
-        operator = "";
-        return screenDisplay.textContent = result;
+        
     } else if (operatorInput === "-") {
         result = reduce(Number(aInput), Number(bInput));
-        a = result;
-        b = Infinity;
-        operator = "";
-        return screenDisplay.textContent = result;
-    } else if (operatorInput === "*") {
+        
+    } else if (operatorInput === "X") {
         result = multiply(Number(aInput), Number(bInput));
-        a = result;
-        b = Infinity;
-        operator = "";
-        return screenDisplay.textContent = result;
+        
     } else if (operatorInput === "/") {
         result = divide(Number(aInput), Number(bInput));
-        a = result;
-        b = Infinity;
-        operator = "";
-        return screenDisplay.textContent = result;
+        
+    } else if (operatorInput === "%") {
+        result = remainder(Number(aInput), Number(bInput));
+       
     } else {
         return screenDisplay.textContent = "ERROR IN THE MATRIX";
     }
+
+    a = result;
+    b = Infinity;
+    operator = "";
+    result = Number(result.toFixed(16));
+
+    screenDisplay.textContent = result.toString();
 }
 
-buttons.forEach(button => button.addEventListener('click', () => {
-    let buttonContent = button.innerText;
+function checkLength() {
+    if ( screenDisplay.textContent.length > 9) {
+        screenDisplay.textContent = screenDisplay.textContent.slice(0, 9)
+    }
+}
 
+document.addEventListener('keypress', (event) => {
+let name = event.key;
+let code = event.code;
+console.log(`Key press ${name} \r\n Key code value : ${code}`);
+
+
+
+if (       name !== "0" 
+        && name !== "1" 
+        && name !== "2" 
+        && name !== "3" 
+        && name !== "4" 
+        && name !== "5" 
+        && name !== "6" 
+        && name !== "7" 
+        && name !== "8" 
+        && name !== "9" 
+        && name !== "/" 
+        && name !=="*" 
+        && name !== "-" 
+        && name !== "+" 
+        && name !== "%"
+        && name !== "Enter" 
+        && name !== "Delete"
+        && name !== "." ){
+    console.log("Typing other key than valid keys.")
+}
+else {
+   let buttonsArray =  Array.from(buttons);
+    let button =  buttonsArray.find((button) => button.id === name)
+        if (button){
+        button.click();
+        }
+    }
+}
+);
+
+
+
+
+buttons.forEach(button => button.addEventListener('click', () => {
+
+
+    let buttonContent = button.innerText;
+    // If clear button clicked reset all var
     if (button.innerText === "CLEAR") {
         screenDisplay.textContent = "";
         a = Infinity;
         operator = "";
         b = Infinity;
         result = 0;
-    } else if (button.classList.contains("btnop")) {
+    } 
+    if ( button.classList.contains("posneg")){
+        if ( screenDisplay.textContent > 0) {
+            screenDisplay.textContent = "-" + screenDisplay.textContent
+        }
+        else if ( screenDisplay.textContent < 0 ) {
+            screenDisplay.textContent = screenDisplay.textContent.slice(1)
+        }
+
+
+    }
+    if ( button.classList.contains("deci")){
+        if (!screenDisplay.textContent.includes(".") && /^\d+(\.\d+)?$/.test(screenDisplay.textContent)){
+            screenDisplay.textContent += buttonContent
+        }
+    }
+    
+    else if (button.classList.contains("btnop")) {
+        // if we can do maths, give result before edit var again
         if (a !== Infinity && b !== Infinity && operator !== "") {
             operate();
-        } else {
+            operator = buttonContent;
+        }
+        // if we can't do maths, we say than var a should have been input just before, so save it and save operator 
+        else {
             a = screenDisplay.textContent;
             screenDisplay.textContent = "" + buttonContent;
             operator = buttonContent;
         }
-    } else if (button.classList.contains("numb")) {
+    } 
+    else if (button.classList.contains("numb")) {
+        // if operator is not empty, we can say we only miss var b
         if (operator !== "") {
+            // we check if b is really empty, we reset screenDisplay to save correctly var b
             if (b === Infinity) {
                 screenDisplay.textContent = "";
             }
             screenDisplay.textContent += buttonContent;
             b = screenDisplay.textContent;
-        } else {
+        }
+        // if operator is empty we display the input user, we're gonna save it while input operator
+         else {
             screenDisplay.textContent += buttonContent;
         }
-    } else if (button.classList.contains("supp")) {
+    }
+    // remove one element of currentDisplay
+    else if (button.classList.contains("supp")) {
         let currentDisplay = screenDisplay.textContent;
         screenDisplay.textContent = currentDisplay.slice(0, -1);
+
+
     } else if (button.classList.contains("total")) {
         operate();
     }
+
+    checkLength();
     console.log("CLICK ON BUTTON");
+
 }));
